3d5c70ff7f0923ad35228b89a7ba01f4a211e134
keshani <keshani.rathnayaka@gmail.com>
2023-03-21 23:53:07 +0200

    Change Urls

--- src/app/common/api-constant.ts
+++ src/app/common/api-constant.ts
@@ -3,9 +3,9 @@ export const FETCH_ALL_USERS = '/userInfoHandler/users';
 export const USER_INFO_FETCH = '/userInfoHandler/users/{userId}';
 export const USER_INFO_ADD = '/userInfoHandler/registerUser';
 export const USER_INFO_UPDATE = '/userInfoHandler/users/{userId}';
-export const ANIMAL_BY_USER_URL = '/animalDetailHandler/animals/{ownerId}';
-export const ANIMAL_ADD_URL = '/animalDetailHandler/animals/{ownerId}';
-export const ANIMAL_UPDATE_URL = '/animalDetailHandler/animals/{ownerId}/{id}';
+export const ANIMAL_BY_USER_URL = '/animalDetailHandler/{ownerId}/animals';
+export const ANIMAL_ADD_URL = '/animalDetailHandler/{ownerId}/animals';
+export const ANIMAL_UPDATE_URL = '/animalDetailHandler/{ownerId}/animals/{id}';
 export const ANIMAL_DELETE_URL = '/animalDetailHandler/animals/{id}';
 export const ANIMAL_STATISTIC = '/animalStatisticHandler/animalsStatistic';
 
